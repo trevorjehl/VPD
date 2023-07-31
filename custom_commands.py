5de1b72f1b755639ffa4f5236e25c4de3600d5e2
trevorjehl <trevorjehl@gmail.com>
2023-07-31 11:37:49 -0700

    Added new files

--- custom_commands.py
+++ custom_commands.py
@@ -1,19 +1,76 @@
 #!/usr/bin/env python3
 """
-Blah
+This file is designed to list out a set of custom 
+G-Code commands for use in wafer scanning with a 
+3D printer.
+
+Created Jul 2023
+by Trevor Jehl
 """
 
 __author__ = "Trevor Jehl"
 __version__ = "0.1.0"
 # __license__ = "MIT"
 
+import os
 from pygcode import *
+from nativeGCodeCommands import *
 
-def main():
-    """ Main entry point of the app """
-    print("hello world")
+# Create a list of all commands
+COMMANDS = []
+
+#ENDER 3 CONSTRAINTS
+X_MAX = 220
+Y_MAX = 220
+Z_MAX = 250
+
+# PROCESS VALUES (in mm unless otherwuise noted)
+EDGE_LENGTH = 5
+XYZ_FEEDRATE = 1125 # Standard is 1125
+E_FEEDRATE = 2700 # Adjust as needed
 
 
-if __name__ == "__main__":
-    """ This is executed when run from the command line """
-    main()
+# Wafer specific global vars (in mm unless otherwuise noted)
+WAFER_DIAM = 101.6 # 4in wafer
+
+
+def startGCode():
+    COMMANDS.append("G21 ; set units to millimeters")
+    COMMANDS.append("M82 ;absolute extrusion mode")
+    COMMANDS.append("G92 E0 ; Reset Extruder")
+    COMMANDS.append("G28 ; Home all axes")
+    COMMANDS.append("G90; Absolute positioning")
+    COMMANDS.append("G92 E0 X0 Y0 Z0; Set home position")
+    #Set extruder feedrate
+    COMMANDS.append(f"G1 F{E_FEEDRATE} E0")
+    #Set XYZ feedrate, move up
+    COMMANDS.append("G1 Z2.0 F{E_FEEDRATE} ; Move up to prevent scrating")
+
+
+def centerHead():
+    nonExtrudeMove(COMMANDS, Z=3)
+    nonExtrudeMove(COMMANDS, X=(X_MAX/2), Y= (Y_MAX)/2)
+    nonExtrudeMove(nonExtrudeMove(COMMANDS, Z=0.2))
+
+
+def write_to_gcode(filename):
+    """
+    At the end of usage, write all commands to a G-code file
+    in the local directory of filename "filename."
+    """
+    # "w" indicates write the new file, "+" Plus sign indicates
+    # both read and write for Python create file operation.
+    f= open(filename + ".gcode","w+")
+    for command in COMMANDS:
+        f.write(command + "\n")
+    
+    # Close the file, writing the lines.
+    f.close()
+
+# def main():
+#     """ Main entry point of the app """
+
+
+# if __name__ == "__main__":
+#     """ This is executed when run from the command line """
+#     main()
---
+++ nativeGCodeCommands.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""
+All relevant native GCODE commands to be 
+called by python.
+
+Created Jul 2023
+by Trevor Jehl
+"""
+
+def nonExtrudeMove(lst,X=None,Y=None,Z=None):
+    """
+    >>> nonExtrudeMove([],1,2,3)
+    ['G0 X1 Y2 Z3']
+    >>> nonExtrudeMove([],X=1,Y=2)
+    ['G0 X1 Y2']
+    >>> nonExtrudeMove([],X=1,Z=3)
+    ['G0 X1 Z3']
+    >>> nonExtrudeMove([],Y=2,Z=3)
+    ['G0 Y2 Z3']
+    >>> nonExtrudeMove([],X=1)
+    ['G0 X1']
+    >>> nonExtrudeMove([],Y=2)
+    ['G0 Y2']
+    >>> nonExtrudeMove([],Z=3)
+    ['G0 Z3']
+    >>> nonExtrudeMove(['test'])
+    ['test']
+    """
+    if X != None and Y != None and Z != None:
+        lst.append(f"G0 X{X} Y{Y} Z{Z}")
+        return lst
+    if X != None and Y != None:
+        lst.append(f"G0 X{X} Y{Y}")
+        return lst
+    if X != None and Z != None:
+        lst.append(f"G0 X{X} Z{Z}")
+        return lst
+    if Y!= None and Z!= None:
+        lst.append(f"G0 Y{Y} Z{Z}")
+        return lst
+    if X!= None:
+        lst.append(f"G0 X{X}")
+        return lst
+    if Y!= None:
+        lst.append(f"G0 Y{Y}")
+        return lst
+    if Z != None:
+        lst.append(f"G0 Z{Z}")
+        return lst
+    return lst
+
+
+def doCircle(lst, xCenterOffset, yCenterOffset):
+    """
+    >>> doCircle([], 20, 20)
+    ['G2 I20 J20']
+    """
+    lst.append(f"G2 I{xCenterOffset} J{yCenterOffset}")
+    return lst
