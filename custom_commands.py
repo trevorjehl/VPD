c9605c0916d245b6b6e417fbe3eb5fa7b0459826
trevorjehl <trevorjehl@gmail.com>
2023-07-31 10:05:42 -0700

    Initial Push

---
+++ custom_commands.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Blah
+"""
+
+__author__ = "Trevor Jehl"
+__version__ = "0.1.0"
+# __license__ = "MIT"
+
+from pygcode import *
+
+def main():
+    """ Main entry point of the app """
+    print("hello world")
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
